4932abf428c77aee0881f22ddedc54bf1dfba907
Wojciech Sikora <wojciech_sikora@epam.com>
2020-06-08 18:26:19 +0200

    Add changes from code review

--- tasks/modules/Modules 2/task_1.py
+++ tasks/modules/Modules 2/task_1.py
@@ -1,38 +1,54 @@
 """This module displays and adds to hero's inventory."""
 
-inventoryDict = {
+from collections import defaultdict
+
+# Program's constants
+MAX_INVENTORY_CAPASITY = 80
+HEAVY_WEIGHT_THRESHOLD = 70
+LIGHT_WEIGHT_THRESHOLD = 60
+RUBBISH = ['chewed gum', 'rubbish', 'used tissue']
+
+inventory_dict = {
     'rope': 1, 'torch': 6, 'gold coin': 42,
     'dagger': 1, 'arrow': 12}
-dragonLootList = [
+dragon_loot_list = [
     'gold coin', 'chewed gum', 'dagger', 'gold coin',
     'gold coin', 'ruby', 'rubbish', 'chewed gum', 'used tissue']
 
 
+def should_skip_item(item):
+    """Check if item is useable for hero's needs"""
+
+    if item in RUBBISH:
+        return True
+    return False
+
+
 def display_inventory(inventory):
-    """Use this function to display hero's inventory.
+    """Display hero's inventory.
 
     This function:
-    -displays all of the inventory items,
-    -sums them
-    -shows warning if hero is carrying too much
+    - displays all of the inventory items
+    - sums them
+    - shows warning if hero is carrying too much
     """
     print("\nInventory:")
     item_total = 0
     for item_name, item_count in inventory.items():
-        item_total = item_total+item_count
-        print("{} {}".format(item_count, item_name))
+        item_total += item_count
+        print(f"{item_count} {item_name}")
 
-    print("\nTotal number of items: {}".format(item_total))
+    print(f"\nTotal number of items: {item_total}")
 
-    if 59 < item_total < 70:
+    if LIGHT_WEIGHT_THRESHOLD <= item_total < HEAVY_WEIGHT_THRESHOLD:
         print(
-            "\nCAUTION: Your backpack weighs a lot,\
- your stamina runs out quicker!")
-    elif 69 < item_total < 80:
+            "\nCAUTION: Your backpack weighs a lot, "
+            "your stamina runs out quicker!")
+    elif HEAVY_WEIGHT_THRESHOLD <= item_total < MAX_INVENTORY_CAPASITY:
         print(
-            "\nCAUTION: Your equipment is very heavy,\
- you are moving slower than usual!")
-    elif item_total > 79:
+            "\nCAUTION: Your equipment is very heavy, "
+            "you are moving slower than usual!")
+    elif item_total >= MAX_INVENTORY_CAPASITY:
         print("\nCAUTION: You are overloaded, can't move!")
 
 
@@ -40,35 +56,33 @@ def add_to_inventory(inventory, added_items):
     """Use this function to add items to hero's inventory.
 
     This function:
-    -adds to inventory valuable items,
-    -throws out rubbish
+    - adds to inventory valuable items
+    - throws out rubbish
     """
-    rubbish = ['chewed gum', 'rubbish', 'used tissue']
-    skipped = {}
+    skipped = defaultdict(int)
     added_items_count = 0
 
     for item in added_items:
-        if item in rubbish:
-            if item in skipped:
-                skipped[item] += 1
-            else:
-                skipped[item] = 1
+        item_not_useable = should_skip_item(item)
+        if item_not_useable is True:
+            skipped[item] += 1
         elif item in inventory:
             inventory[item] += 1
             added_items_count += 1
-        elif item not in inventory and item not in rubbish:
+        else:
             inventory[item] = 1
+            added_items_count += 1
             print(
-                "\n\nFound new item !{}! => adding to inventory!\n"
-                .format(item))
+                f"\n\nFound new item !{item}! => adding to inventory!\n"
+                )
 
-    print('Added {} items to the inventory'.format(added_items_count))
+    print(f"Added {added_items_count} items to the inventory")
     print("\nSkipped:")
     for item_name, item_count in skipped.items():
-        print("{} {}".format(item_count, item_name))
+        print(f"{item_count} {item_name}")
 
     return inventory
 
 
-inventoryDict = add_to_inventory(inventoryDict, dragonLootList)
-display_inventory(inventoryDict)
+inventory_dict = add_to_inventory(inventory_dict, dragon_loot_list)
+display_inventory(inventory_dict)
