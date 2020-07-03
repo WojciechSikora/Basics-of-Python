edf98cfde4cc16419d5e04e471ad70787667b8ec
Wojciech Sikora <wojciech_sikora@epam.com>
2020-07-03 15:20:02 +0200

    Add code for task_1 and files for rest tasks

---
+++ tasks/modules/Module 4/task_1.py
@@ -0,0 +1,40 @@
+"""Find string and replace.
+
+Finds 'ADJECTIVE', 'NOUN', 'ADVERB', or 'VERB' occurrences in text file
+and allows to replace them with chosen strings.
+"""
+
+from pathlib import Path
+from re import search, sub
+from pyinputplus import inputStr
+
+
+# Program CONSTANTS
+STRINGS_TO_FIND = [
+    'ADJECTIVE', 'NOUN', 'ADVERB', 'VERB'
+]
+
+
+def mad_libs(file_path, list_of_patterns):
+    """Search for pattern from list and replace with user's string."""
+    path_to_rw_files = Path(file_path)
+    with open(path_to_rw_files, 'rt') as my_file:
+        text_file_content = my_file.read()
+    for pattern in list_of_patterns:
+        search_result = search(pattern, text_file_content)
+        while search_result:
+            replacement = inputStr(
+                f'Enter new word to replace "{pattern}":')
+            text_file_content = sub(
+                pattern, replacement, text_file_content, count=1)
+            search_result = search(pattern, text_file_content)
+    print(text_file_content)
+    new_text_file = open(
+        fr'{path_to_rw_files.absolute().parent}/{path_to_rw_files.stem}__CHANGED.txt', 'w')
+    new_text_file.write(text_file_content)
+    new_text_file.close()
+
+
+if __name__ == '__main__':
+    mad_libs(
+        'tasks/modules/Module 4/task_1_files/text_file.txt', STRINGS_TO_FIND)
