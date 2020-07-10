79b2a617e4e6bd996fd5068d465d6fc1bef66b84
Wojciech Sikora <wojciech_sikora@epam.com>
2020-07-10 16:05:32 +0200

    Add task_2.py

---
+++ tasks/modules/Module 4/task_2.py
@@ -0,0 +1,29 @@
+"""Checks files naming and renames if gaps present."""
+
+
+from pathlib import Path
+from re import search
+import shutil
+from pyinputplus import inputStr
+
+
+def fill_the_gaps(prefix, file_type="txt"):
+    """Fix indexes in files with same prefix in name."""
+    path_to_folder = inputStr("Provide absolute path to folder: ")
+    data_folder = Path(path_to_folder)
+    files_in_folder = sorted(data_folder.rglob(f'{prefix}*.{file_type}'))
+    if not files_in_folder:
+        return "No files with given prefix and/or type in chosen folder"
+    number_len = len(search(r'\d+', files_in_folder[0].stem).group())
+    sorting_number = int(search(r'\d+', files_in_folder[0].stem).group())
+    for file in files_in_folder:
+        file_index = int(search(r'\d+', file.stem).group())
+        if file_index != sorting_number:
+            changed_name_path = data_folder / f"{prefix}{sorting_number:0{number_len}d}.txt"
+            shutil.move(file, changed_name_path)
+        sorting_number += 1
+    return sorted(data_folder.rglob(f'{prefix}*.{file_type}'))
+
+
+if __name__ == '__main__':
+    fill_the_gaps("spam")
