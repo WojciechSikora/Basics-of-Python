f0dcd9b0902528274a07e1d008c607211ba7a985
Wojciech Sikora <wojciech_sikora@epam.com>
2020-08-09 20:48:29 +0200

    Change logic in task 1

--- tasks/modules/Module 4/task_1.py
+++ tasks/modules/Module 4/task_1.py
@@ -5,34 +5,24 @@ and allows to replace them with chosen strings.
 """
 
 from pathlib import Path
-from re import search, sub
+import re
 from pyinputplus import inputStr
 
 
-# Program CONSTANTS
-STRINGS_TO_FIND = [
-    'ADJECTIVE', 'NOUN', 'ADVERB', 'VERB'
-]
-
-
-def mad_libs(file_path, list_of_patterns):
-    """Search for pattern from list and replace with user's string."""
+def mad_libs(file_path):
+    """Search for pattern from list and replace with user's input."""
     path_to_rw_files = Path(file_path)
     with open(path_to_rw_files, 'rt') as my_file:
         text_file_content = my_file.read()
-    for pattern in list_of_patterns:
-        search_result = search(pattern, text_file_content)
-        while search_result:
-            replacement = inputStr(
-                f'Enter new word to replace "{pattern}":')
-            text_file_content = sub(
-                pattern, replacement, text_file_content, count=1)
-            search_result = search(pattern, text_file_content)
-    print(text_file_content)
+    pattern = re.compile(r'\b(ADJECTIVE|NOUN|ADVERB|VERB)\b')
+    for match in pattern.finditer(text_file_content):
+        replacement = inputStr(f'Enter new word to replace "{match.group(0)}":')
+        text_file_content = re.sub(match.group(0), replacement, text_file_content, 1)
+    print(f'Changed text:\n{text_file_content}')
     with open(fr'{path_to_rw_files.absolute().parent}/{path_to_rw_files.stem}__CHANGED.txt', 'w') as new_text_file:
         new_text_file.write(text_file_content)
 
 
 if __name__ == '__main__':
     mad_libs(
-        'tasks/modules/Module 4/task_1_files/text_file.txt', STRINGS_TO_FIND)
+        'tasks/modules/Module 4/task_1_files/text_file.txt')
